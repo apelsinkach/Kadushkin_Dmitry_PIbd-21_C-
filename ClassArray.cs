adf8b5acc518cd955e8f22942ce16ac7d45d38ae
Dmitriy <apelsinka.channel@gmail.com>
2017-12-17 14:43:10 +0400

    Add files via upload

--- ClassArray.cs
+++ ClassArray.cs
@@ -6,70 +6,71 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class ClassArray<T> where T:ITransport
+    class ClassArray<T> where T : ITransport
     {
-        private T[] places;
+        private Dictionary<int, T> places;
+
+        private int maxCount;
         private T defaultValue;
 
-        public ClassArray(int sizes,T defVal)
+        public ClassArray(int sizes, T defVal)
         {
             defaultValue = defVal;
-            places = new T[sizes];
-            for(int i = 0; i < places.Length; i++)
-            {
-                places[i] = defaultValue;
-            }
+            places = new Dictionary<int, T>();
+            maxCount = sizes;
+         
         }
 
 
         public static int operator +(ClassArray<T> p, T locomotive)
         {
-            for (int i = 0; i < p.places.Length; i++)
+            if (p.places.Count == p.maxCount)
+            {
+                return -1;
+            }
+            for (int i = 0; i < p.places.Count; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p.places[i] = locomotive;
+                    p.places.Add(i, locomotive);
                     return i;
                 }
             }
-            return -1;
+            p.places.Add(p.places.Count, locomotive);
+            return p.places.Count - 1;
+    
         }
 
         public static T operator -(ClassArray<T> p, int index)
         {
-            if (!p.CheckFreePlace(index))
+            if (p.places.ContainsKey(index))
             {
                 T locomotive = p.places[index];
-                p.places[index] = p.defaultValue;
+                p.places.Remove(index);
                 return locomotive;
             }
             return p.defaultValue;
-        }
-   
-        private bool CheckFreePlace(int index)
-        {
-            if (index < 0 || index > places.Length)
-            {
-                return false;
-            }
-            if (places[index] == null)
-            {
-                return true;
-            }
-            if (places[index].Equals(defaultValue))
-            {
-                return true;
-            }
-            return false;
+
+      
         }
 
-        public T getObject(int ind)
+        private bool CheckFreePlace(int index)
         {
-            if(ind>-1 && ind < places.Length)
+
+            return !places.ContainsKey(index);        
+        }
+
+        public T this[int ind]
+        {
+            get
             {
-                return places[ind];
+
+                if (places.ContainsKey(ind))
+                {
+                    return places[ind];
+                }
+                return defaultValue;
             }
-            return defaultValue;
         }
     }
 }
